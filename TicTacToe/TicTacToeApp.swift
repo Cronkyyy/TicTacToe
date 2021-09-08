fb5bb719f3ecd175f65311710b595abcdce57bf4
Dylan Cronk <cronkyyy@gmail.com>
2021-09-08 13:04:23 -0400

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Dylan Cronk on 9/8/21.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
